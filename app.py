7f80b4c0a4a7c17cb9167b7a31e714660231872d
Ryan Mills <ryandarylmills@gmail.com>
2019-02-13 12:29:37 +1100

    Narrow polygon instead of line.

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from PIL import Image
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+def boundary_to_polygon(boundary):
+  boundary = boundary + [(p[0] - 1, p[1]) for p in reversed(boundary)]
+  polygon = mapping(Polygon(boundary).simplify(1))['coordinates'][0][:-1]
+  return polygon
+
 with open('boundaries_by_hash.json') as fh:
   for i in fh:
     boundaries_by_hash = json.loads(i)
@@ -16,8 +21,8 @@ def inference():
   response = requests.get(url)
   image = Image.open(BytesIO(response.content))
   hash = hashlib.md5(image.tobytes()).hexdigest()
-  boundaries = ['ILM (ILM)', 'Inner boundary of RPE (IB_RPE)']
-  ilm, inner_rpe = [mapping(Polygon(boundaries_by_hash[hash][i]).simplify(1))['coordinates'][0][:-1] for i in boundaries]
+  boundary_names = ['ILM (ILM)', 'Inner boundary of RPE (IB_RPE)']
+  ilm, inner_rpe = [boundary_to_polygon(boundaries_by_hash[hash][i]) for i in boundary_names]
   predictions = [
     {
       "class": "ILM",
